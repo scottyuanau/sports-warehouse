cace251ae5a2cc29a57748cb0eb14149c8582469
Scott Yuan <scoworthyuan@gmail.com>
2023-03-18 09:13:46 +1100

    project init added nav

--- index.php
+++ index.php
@@ -5,13 +5,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href="css/index.css" />
     <title>Sports Warehouse</title>
 </head>
 
 <body>
-    <?php
-    echo "hello";
-    ?>
+    <main>
+        <header>
+            <img src='img/logo.png' alt='logo'>
+            <div class='searchbar'>
+                <input type='text' placeholder="Search products" class="searchbar-input" />
+                <div class="searchbar-icon">
+                    <i class="fa-solid fa-magnifying-glass searchbar-icon_inside"></i>
+                </div>
+            </div>
+        </header>
+        <nav class="headernav">
+            <ul class='headernav-ul'>
+                <li class="headernav-li">Shoes</li>
+                <li class="headernav-li">Helmets</li>
+                <li class="headernav-li">Pants</li>
+                <li class="headernav-li">Tops</li>
+                <li class="headernav-li">Balls</li>
+                <li class="headernav-li">Equipment</li>
+                <li class="headernav-li">Training gear</li>
+            </ul>
+        </nav>
+
+    </main>
+    <footer>
+
+    </footer>
+
+
+    <script src="https://kit.fontawesome.com/b0c79d8457.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
